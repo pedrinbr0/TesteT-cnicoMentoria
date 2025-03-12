da23fc9508ebfc8aa68d1c87e57ad37a2bb1eaef
pedro <paulosilvaotaviano@gmail.com>
2025-03-12 16:11:02 -0300

    Respondendo pergunta da questão 6

---
+++ TT1/Questão6.rb
@@ -0,0 +1,7 @@
+#Descreva o que faz o seguinte comando: a ||= b
+
+Se "a" já tem um valor, mantém como está. Se "a" estiver vazio (nil ou false), coloca "b" no lugar.
+
+#e por que ele é amplamente utilizado?
+
+Porque podemos usar para definir valores padrão e para garantir que uma variável tenha um valor sem substituir um valor já existente.
