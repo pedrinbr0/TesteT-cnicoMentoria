13e799ccf0790b232c195f96f1ad6a3589df2d74
pedro <paulosilvaotaviano@gmail.com>
2025-03-12 15:58:11 -0300

    Sintaxe ActiveRecord questão 5

--- TT1/Questão5.rb
+++ TT1/Questão5.rb
@@ -12,3 +12,18 @@ end
 class JobTitle < ApplicationRecord
   has_many :employees
 end
+
+#Sintaxe do ActiveRecord
+
+employees = Employee
+  .joins("LEFT JOIN teams ON employees.team_id = teams.id")
+  .joins("LEFT JOIN job_titles ON employees.job_title_id = job_titles.id")
+  .select("employees.first_name || ' ' || employees.last_name AS nome_completo,
+           employees.cpf,
+           teams.name AS equipe,
+           job_titles.name AS cargo")
+  .order("employees.cpf")
+
+employees.each do |employee|
+  puts "#{employee.nome_completo} | #{employee.cpf} | #{employee.equipe || 'Sem equipe'} | #{employee.cargo}"
+end
